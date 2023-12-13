ead44ddc4f2cd6dcae3d3d64c4a04b01d6104ae8
sempaii123 <118355971+sempaii123@users.noreply.github.com>
2023-12-13 09:29:42 -0400

    Add files via upload

--- database-memory.js
+++ database-memory.js
@@ -1,40 +1,38 @@
 import { randomUUID } from "crypto"
 
+export class DatabaseMemory {
+  #tenis = new Map()
 
-export class DatabaseMemory{
-    #loja_de_sapatos = new Map()
-
-list(search){
-    return Array.from(this.#loja_de_sapatos.entries()).map((tenisArray) => {
+  list(search) {
+    return Array.from(this.#tenis.entries())
+      .map((tenisArray) => {
         const id = tenisArray[0]
 
         const data = tenisArray[1]
 
-        return{
-            id,
-            ...data,
+        return {
+          id,
+          ...data,
         }
+      })
+      .filter((tenis) => {
+        if (search) {
+          return tenis.modelo.includes(search)
+        }
+        return true
+      })
+  }
 
-        
-        })
-        .filter(loja_de_sapatos=> {
-            if (search){
-            return loja_de_sapatos.titulo.includes(search)
-            }
-            return true
-    })
-}
+  create(tenis) {
+    const tenisId = randomUUID()
+    this.#tenis.set(tenisId, tenis)
+  }
 
-    create(livrloja_de_sapatos){
-        const loja_de_sapatosId = randomUUID()
-        this.#loja_de_sapatos.set(loja_de_sapatosId, loja_de_sapatos)
-    }
-    
-    update(id, loja_de_sapatos){
-        this.#loja_de_sapatos.set(id, loja_de_sapatos)
-    }
+  update(id, tenis) {
+    this.#tenis.set(id, tenis)
+  }
 
-    delete(id, loja_de_sapatos){
-        this.#loja_de_sapatos.delete(id, loja_de_sapatos)
-    }
+  delete(id, tenis) {
+    this.#tenis.delete(id, tenis)
+  }
 }
--- server.js
+++ server.js
@@ -1,54 +1,49 @@
-import { fastify } from 'fastify'
-import {DatabaseMemory} from "./database-memory.js"
+import { fastify } from "fastify"
+import { DatabaseMemory } from "./database-memory.js"
 
 const server = fastify()
 const database = new DatabaseMemory()
 
-server.get('/', () => {
-    return 'Olá Mundo'
+server.post("/tenis", (request, reply) => {
+  const { modelo, marca, pares } = request.body
+  database.create({
+    modelo: modelo,
+    marca: marca,
+    pares: pares,
+  })
+  console.log(database.list())
+  return reply.status(201).send()
 })
 
-server.post('/tenis', (request, reply) => {
-    //const body = request.body//
-   //console.log(body)//
-   const {modelo, marca, pares } = request.body
-    database.create({
-        modelo: modelo,
-        marca: marca,
-        pares: pares
-    })
-    //console.log(database.list())
-    return reply.status(201).send()
+server.get("/tenis", (request) => {
+  const search = request.query.search
+
+  console.log(search)
+
+  const tenis = database.list(search)
+
+  return tenis
 })
 
-server.get('/tenis', (request) => {
-    const search = request.query.search
-
-    const tenis = database.list(search)
-
-    return tenis
+server.put("/tenis/:id", (request, reply) => {
+  const tenisId = request.params.id
+  const { modelo, marca, pares } = request.body
+  const tenis = database.update(tenisId, {
+    modelo,
+    marca,
+    pares,
+  })
+  return reply.status(204).send()
 })
 
-server.put('tenis/:id', (request, reply) => {
+server.delete("/tenis/:id", (request, reply) => {
+  const tenisId = request.params.id
 
-    const tenisId = request.params.id
-    const {modelo, marca, pares} = request.body
-    const livro = database.update(tenisId, {
-        modelo,
-        marca,
-        pares,
-    })
-    return reply.status(204).send()
-})
+  database.delete(tenisId)
 
-server.delete('/tenis/:id', (request, reply) => {
-    const livroId = request.params.id
-
-    database.delete(tenisId)
-
-    return reply.status(204).send()
+  return reply.status(204).send()
 })
 
 server.listen({
-    port: 3333,
+  port: 3333,
 })
